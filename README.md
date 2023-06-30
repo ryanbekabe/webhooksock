b2a3ad7e174549d97c07e214f63bdeffef6eb036
root <root@vmi1099311.contaboserver.net>
2023-06-30 20:06:17 +0700

    WebHookSock 30062023

---
+++ README.md
@@ -0,0 +1,7 @@
+# webhooksock
+git init
+git add .
+git commit -m "WebHookSock 30062023"
+git branch -M main
+git remote add origin git@github.com:ryanbekabe/webhooksock.git
+git push -u origin main
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.post('/webhook', (req, res) => {
+  const eventData = req.body;
+  io.emit('notification', eventData);
+  res.sendStatus(200);
+});
+
+const port = 3006;
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
